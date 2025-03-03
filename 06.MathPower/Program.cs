c881561526f9689d8b66f7d60603749377e77d21
rebelwithoutacause <148477548+rebelwithoutacause@users.noreply.github.com>
2025-03-03 21:44:32 +0200

    Add files via upload

---
+++ 06.MathPower/Program.cs
@@ -0,0 +1,15 @@
+﻿int numberOne = int.Parse(Console.ReadLine());
+int numberTwo = int.Parse(Console.ReadLine());
+
+Console.WriteLine(MathPower(numberOne, numberTwo));
+
+static int MathPower(int numberOne, int numberTwo) 
+{
+    int result = 1;
+    
+    for (int i = 1; i <= numberTwo; i++) 
+    {
+        result *= numberOne; 
+    }
+    return result;
+}

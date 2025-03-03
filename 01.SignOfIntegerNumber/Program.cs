8c5d340113752c841b298ea5ccb768f1913447d8
rebelwithoutacause <148477548+rebelwithoutacause@users.noreply.github.com>
2025-03-03 21:36:51 +0200

    Add files via upload

---
+++ 01.SignOfIntegerNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine());
+PrintSign(n);
+static void PrintSign(int number) 
+{
+    if (number < 0) 
+    {
+        Console.WriteLine($"The number {number} is negative.");
+    }
+    else if (number > 0) 
+    {
+        Console.WriteLine($"The number {number} is positive.");
+    }
+    else 
+    {
+    Console.WriteLine($"The number {number} is zero.");
+    }
+}
---
+++ 03.PrintingTriangle/Program.cs
@@ -0,0 +1,27 @@
+﻿//Write a program that:
+//•	Reads an integer number N from the console
+//•	Create a method for printing triangle depending on value of the number N
+
+int num = int.Parse(Console.ReadLine());
+
+PrintingTriangle(num);
+
+static void PrintingTriangle(int num) 
+{
+    for (int row  = 1; row < num; row++) 
+    {
+    PrintLine(1, row);
+    }
+    for (int row = num; row >= 1; row--) 
+    {
+        PrintLine (1, row);
+    }
+}
+static void PrintLine(int start, int end) 
+{
+for (int i = start; i <=end; i++) 
+    {
+        Console.Write($"{i} ");
+    }
+Console.WriteLine();
+}
